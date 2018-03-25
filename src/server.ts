ecff86643c2ee42fa8efcf8193faecf5867c9688
Jacek Czastkiewicz <jc@archlab.pl>
2018-03-25 15:13:22 -0400

    adding mqtt

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,31 @@
 import app from "./app";
 
+import mqtt from "mqtt";
+
+/**
+ * Start mqtt sub
+ */
+const client = mqtt.connect({
+  host: process.env.MQTT_HOST,
+  port: process.env.MQTT_PORT,
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD,
+});
+
+client.on('connect', function () {
+  client.subscribe('jczas/feeds/jenkins')
+});
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+});
+
+client.on('error', function (error) {
+  console.log('error: ' + error)
+});
+
+
 /**
  * Start Express server.
  */
@@ -11,5 +37,6 @@ const server = app.listen(app.get("port"), () => {
   );
   console.log("  Press CTRL-C to stop\n");
 });
+
 
 export default server;
