def932b00352154aa2cfa96655c7e7a09f46fbd7
Jacek Czastkiewicz <jc@archlab.pl>
2018-03-25 14:36:00 -0400

    removing not needed packages

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,6 @@
 import express from "express";
-import bodyParser from "body-parser";
-import lusca from "lusca";
 import dotenv from "dotenv";
 import path from "path";
-import expressValidator from "express-validator";
 
 // Load environment variables from .env file, where API keys and passwords are configured
 dotenv.config({ path: ".env.example" });
@@ -18,11 +15,6 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressValidator());
-app.use(lusca.xframe("SAMEORIGIN"));
-app.use(lusca.xssProtection(true));
 
 app.use(
   express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
